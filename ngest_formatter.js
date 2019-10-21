c5f05212df671673beb4aa5276f815ad95495c1e
jeffdyoung <jeff@ndustrial.io>
2019-10-21 16:51:35 -0400

    beta

---
+++ ngest_formatter.js
@@ -0,0 +1,35 @@
+const sdk = require('@ndustrial/contxt-sdk');
+
+module.exports = function (RED) {
+    function NgestFormatterNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            var ngestmsg = {};
+            ngestmsg.payload = {};
+            ngestmsg.payload.type = "timeseries";
+            //set timestamp to now
+            var thistimestamp = Date.now().toString();
+
+            msg.payload.forEach(function (element) {
+                //grab timestamp, reformat and tag
+                element.timestamp = thistimestamp;
+                //id = tagname
+                //v = value
+                //q = quality = updates every scan
+                //t = timestamp
+                //re-write id/value to match ngest format
+                var data = "{\"" + element.name.toString() + "\": {\"value\": \"" + element.value.toString() + "\"}}";
+                element.data = JSON.parse(data);
+                //element.data = data;
+                //remove original elements 
+                delete element.value;
+                delete element.name;
+            });
+            ngestmsg.payload.data = msg.payload;
+            ngestmsg.payload=JSON.stringify(ngestmsg.payload);
+            node.send(ngestmsg);
+        });
+    }
+    RED.nodes.registerType("nGest Formatter", NgestFormatterNode);
+}
